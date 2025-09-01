2f933ffccbcbd3eae540b09d9d3ebfbc948a62f4
vilchez <dievilchez23@gmail.com>
2025-08-31 22:16:14 -0600

    add base code for cpu filtering

---
+++ image_filttering.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main () {
+    image.open()
+    new.image.open()
+
+    string type = "", width = "", height = "", RGB = "";
+    image >> type;
+    image >> width;
+    image >> height;
+    image >> RGB;
+
+    newimage << type << endl;
+    newimage << width << "" << height endl;
+    newimage << RGB << endl;
+
+    return 0; 
+}
