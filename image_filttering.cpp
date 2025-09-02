0f1df79f45de28a048600135800c2feb19defa9f
vilchez <dievilchez23@gmail.com>
2025-09-01 18:03:24 -0600

    funtion to calculate grid average

--- image_filttering.cpp
+++ image_filttering.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
-#include <fstream>
-
 using namespace std;
 
-int main () {
-    image.open()
-    new.image.open()
+void cpu_blur(int height, int width, int* in) {
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            int blur_sum = 0;
+            for (int grid_y = -1; grid_y < 2; grid_y++) {
+                for (int grid_x = -1; grid_x < 2; grid_x++) {
+                    int blur_y = y + grid_y;
+                    int blur_x = x + grid_x;
+                    if (blur_y >= 0 && blur_x >= 0 && blur_y < height && blur_x < width) {
+                        blur_sum += in[blur_y * width + blur_x];
+                        }
+                }
+                cout << blur_sum << endl; 
+            }
+        }  
+    }      
+}
 
-    string type = "", width = "", height = "", RGB = "";
-    image >> type;
-    image >> width;
-    image >> height;
-    image >> RGB;
+int main() {
+    const int width = 5, height = 5;
+    int input[width * height] = {
+        10, 20, 30, 40, 50,
+        60, 70, 80, 90,100,
+        110,120,130,140,150,
+        160,170,180,190,200,
+        210,220,230,240,250
+    };
+    unsigned char output[width * height];
 
-    newimage << type << endl;
-    newimage << width << "" << height endl;
-    newimage << RGB << endl;
-
-    return 0; 
+    cpu_blur(height, width, input);
+    return 0;
 }
