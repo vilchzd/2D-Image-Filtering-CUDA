0520817e1bd052fa41c4c94ec05fb70881e9aa5d
vilchez <dievilchez23@gmail.com>
2025-09-01 20:14:08 -0600

    3x3 blur implemented and tested on sample matrix

--- image_filttering.cpp
+++ image_filttering.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 using namespace std;
 
-void cpu_blur(int height, int width, int* in) {
+void cpu_blur(int height, int width, int* in, int* out) {
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             int blur_sum = 0;
             int count = 0;
+            int average = 0;
             for (int grid_y = -1; grid_y < 2; grid_y++) {
                 for (int grid_x = -1; grid_x < 2; grid_x++) {
                     int blur_y = y + grid_y;
                     int blur_x = x + grid_x;
                     if (blur_y >= 0 && blur_x >= 0 && blur_y < height && blur_x < width) {
                         blur_sum += in[blur_y * width + blur_x];
+                        count++;
                         }
-                    count =+ blur_sum;
                 }
-
             }
-            cout << count << endl;
+            average = blur_sum / count;
+            out[y * width + x] = average;
         }  
     }      
+    for (int yo = 0; yo < height; yo++) {
+        for (int xo = 0; xo < width; xo++) {
+            cout << out[yo * width + xo] << " ";
+        }
+        cout << endl;
+    }
 }
 
 int main() {
@@ -31,8 +38,7 @@ int main() {
         160,170,180,190,200,
         210,220,230,240,250
     };
-    unsigned char output[width * height];
-
-    cpu_blur(height, width, input);
+    int output[width * height];
+    cpu_blur(height, width, input, output);
     return 0;
 }
