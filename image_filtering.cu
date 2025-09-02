be48b4461f9797392887b3e456ec6d95479e72b8
vilchez <dievilchez23@gmail.com>
2025-09-02 05:56:43 -0600

    Implement cuda matrix kernel with shared memory

--- image_filtering.cu
+++ image_filtering.cu
@@ -1,86 +1,71 @@
-// int i = threadIdx.x + blockDim.x * blockIdx.x
 #include <iostream>   
 #include <cstdlib>   
-#include <iomanip>
 #include <cuda_runtime.h>
-
-#define N 32
-#define BLOCK_SIZE 16
 
 using namespace std;
 
-__global__ void transpose(float *in, float *out, int width) {
-    __shared__ float t_mat[BLOCK_SIZE][BLOCK_SIZE];
-
-    int i = threadIdx.x + BLOCK_SIZE * blockIdx.x ;
-    int j = threadIdx.y + BLOCK_SIZE * blockIdx.y ;
-    
-
-    if (i < width && j < width) {
-        t_mat[threadIdx.y][threadIdx.x] = in[j * width + i];
-    }
-
+__global__ void matrix(float *in, float *out, int width, int height) {
+    extern __shared__ float mat[];
+    int x = threadIdx.x;
+    int y = threadIdx.y;
+    mat[x * width + y] = in[x * width + y]; 
     __syncthreads();
-
-    int trans_i = threadIdx.x + BLOCK_SIZE * blockIdx.y;
-    int trans_j = threadIdx.y + BLOCK_SIZE * blockIdx.x;
-
-
-    if (trans_i < width && trans_j < width) {
-        out[trans_j * width + trans_i] = t_mat[threadIdx.x][threadIdx.y];
-    }
+    mat[x * width + y] /= 10; 
+    out[x * width + y] = mat[x * width + y]; 
 }
 
-
 int main() {
-
-    int width = N;
-    float *d_in, *d_out;
-    float *h_in = (float*)malloc(width*width*sizeof(float));
-    float *h_out = (float*)malloc(width*width*sizeof(float));
-
-    for (int i = 0; i < width*width; i++) {
-        h_in[i] = i+1;
+    const int width = 5, height = 5;
+    float *d_in, *d_out; 
+    float *h_in = (float*)malloc(height*width*sizeof(float));
+    float *h_out = (float*)malloc(height*width*sizeof(float));
+    cudaMalloc((void**)&d_in,width*height*sizeof(float));
+    cudaMalloc((void**)&d_out,width*height*sizeof(float));
+    
+    for (int i = 0; i < height*width; i++) {
+        h_in[i] = i*10+10;
     }
 
-    cudaMalloc((void**)&d_in, width*width*sizeof(float));
-    cudaMemcpy(d_in, h_in, width*width*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMalloc((void**)&d_out, width*width*sizeof(float));
+    cudaMemcpy(d_in, h_in, width*height*sizeof(float), cudaMemcpyHostToDevice);
     
+    dim3 grid_size(1);
+    dim3 block_size(width,height);
+    size_t shm_size = width * height * sizeof(float);
+    matrix<<<grid_size,block_size, shm_size>>>(d_in, d_out, 5 , 5);
     
-    dim3 block_size(BLOCK_SIZE, BLOCK_SIZE);
-    dim3 grid_size((N + BLOCK_SIZE - 1)/BLOCK_SIZE, (N + BLOCK_SIZE - 1)/BLOCK_SIZE);
-
-      
-    cudaEvent_t gpu_start, gpu_stop;
-    cudaEventCreate(&gpu_start);
-    cudaEventCreate(&gpu_stop);
-    cudaEventRecord(gpu_start);
-
-    transpose<<<grid_size, block_size>>>(d_in, d_out, N);
-    cudaDeviceSynchronize();
+    cudaMemcpy(h_out, d_out, width*height*sizeof(float), cudaMemcpyDeviceToHost);
     
-    
-    cudaEventRecord(gpu_stop);
-    cudaEventSynchronize(gpu_stop);
-
-    cudaMemcpy(h_out, d_out, width*width*sizeof(float), cudaMemcpyDeviceToHost);
-
-    for(int i=0;i<width;i++){
-        for(int j=0;j<width;j++)
-            cout << h_out[i*width+j] << " ";
+    for (int y = 0; y < height; y++) {
+        for (int i = 0; i < width; i++) {
+            cout << h_out[y*width + i] << " ";
+        }
         cout << endl;
     }
-
-
-    float milliseconds = 0;
-    cudaEventElapsedTime(&milliseconds, gpu_start, gpu_stop);
-    cout << "GPU time: " << milliseconds << " ms" << endl;
-
-
     free(h_in);
     free(h_out);
     cudaFree(d_in);
     cudaFree(d_out);
+    
     return 0;
-} 
+}
+
+/* __global__ void read_m(float *in, float *out, int w, int h) {
+    const int height = 5;
+    const int width = 5;
+    __shared__ float matrix[height][width];
+
+    int y = threadIdx.y;
+    int x = threadIdx.x;
+    if ()
+    
+
+    if (i < width && j < width) {
+        matrix[threadIdx.y][threadIdx.x] = in[j * width + i];
+    }
+
+    __syncthreads();
+    if (trans_i < width && trans_j < width) {
+        out[trans_j * width + trans_i] = matrix[threadIdx.x][threadIdx.y];
+    }
+}
+*/
